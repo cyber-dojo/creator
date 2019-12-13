3b6e91e70742b951fa6e420887516d73a8b36291
JonJagger <jon@jaggersoft.com>
2019-12-13 14:11:53 +0000

    Fix coverage grouping

--- test_server/util/coverage.rb
+++ test_server/util/coverage.rb
@@ -2,8 +2,13 @@ require 'simplecov'
 
 app_root = File.expand_path('../..', __dir__)
 SimpleCov.start do
-  add_group( 'src') { | src|  src.filename.start_with?("#{app_root}" ) }
-  add_group('test') { |test| test.filename.start_with?("#{app_root}/test") }
+  add_group( 'src') { | src|
+    src.filename.start_with?("#{app_root}") &&
+    !src.filename.start_with?("#{app_root}/test")      
+  }
+  add_group('test') { |test|
+    test.filename.start_with?("#{app_root}/test")
+  }
 end
 SimpleCov.root(app_root)
 SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
