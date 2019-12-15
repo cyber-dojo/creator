af941c8cec0ab1d3a1d0fb587fb3c36991ce5569
JonJagger <jon@jaggersoft.com>
2019-12-15 14:52:53 +0000

    Update demo script to show all paths

--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly PORT=4523
-
-export $(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env')
-
 ip_address()
 {
   if [ -n "${DOCKER_MACHINE_NAME}" ]; then
@@ -15,6 +10,48 @@ ip_address()
   fi
 }
 
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+readonly IP_ADDRESS=$(ip_address)
+readonly PORT=4523
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+export $(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env')
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
-open "http://$(ip_address):${PORT}/alive?"
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+curl_json()
+{
+  local -r TYPE=${1}
+  local -r ROUTE=${2}
+  # TODO: get real manifest from custom-start-points
+  curl  \
+    --data '{"manifest":{}}' \
+    --fail \
+    --header 'Accept: application/json' \
+    --silent \
+    -X ${TYPE} \
+    "http://${IP_ADDRESS}:${PORT}/${ROUTE}"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+demo_api()
+{
+  printf "\n"
+  printf "API\n"
+  printf "\t200 GET  /alive? => $(curl_json GET alive?)\n"
+  printf "\t200 GET  /ready? => $(curl_json GET ready?)\n"
+  printf "\t200 GET  /sha    => $(curl_json GET sha)\n"
+  printf "\t200 POST /create_group => $(curl_json POST create_group)\n"
+  printf "\t200 POST /create_kata  => $(curl_json POST create_kata)\n"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+open_alive_in_browser()
+{
+  printf "\n"
+  open "http://${IP_ADDRESS}:${PORT}/alive?"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+demo_api
+open_alive_in_browser
