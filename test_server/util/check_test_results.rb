71459af59148daafe8b78a14b8281015884f1d87
JonJagger <jon@jaggersoft.com>
2019-12-17 09:51:31 +0000

    Refactoring; rename

--- test_server/util/check_test_results.rb
+++ test_server/util/check_test_results.rb
@@ -83,8 +83,8 @@ end
 # - - - - - - - - - - - - - - - - - - - - - - -
 
 log_stats = get_test_log_stats
- test_stats = get_index_stats('test')
- src_stats = get_index_stats('source')
+test_stats = get_index_stats('test')
+source_stats = get_index_stats('source')
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -95,11 +95,11 @@ warning_count = log_stats[:warning_count]
 skip_count    = log_stats[:skip_count]
 test_duration = log_stats[:time].to_f
 
-src_coverage  = src_stats[:coverage].to_f
+source_coverage  = source_stats[:coverage].to_f
 test_coverage = test_stats[:coverage].to_f
 
-line_ratio    = (test_stats[:line_count].to_f / src_stats[:line_count].to_f)
-hits_ratio    = (src_stats[:hits_per_line].to_f / test_stats[:hits_per_line].to_f)
+line_ratio    = (test_stats[:line_count].to_f / source_stats[:line_count].to_f)
+hits_ratio    = (source_stats[:hits_per_line].to_f / test_stats[:hits_per_line].to_f)
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -111,10 +111,10 @@ table =
     [ 'warnings',               warning_count,      '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
     [ 'duration(test)[s]',      test_duration,      '<=',   2 ],
-    [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
+    [ 'coverage(source)[%]',    source_coverage,    '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.5 ],
-    [ 'hits(src)/hits(test)',   f2(hits_ratio),     '>=', 2.2 ],
+    [ 'lines(test/source)',     f2(line_ratio),     '>=', 1.5 ],
+    [ 'line-hits(source/test)', f2(hits_ratio),     '>=', 2.2 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
