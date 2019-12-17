d3843eb5262d64daf051363154747b702a309880
JonJagger <jon@jaggersoft.com>
2019-12-17 09:38:31 +0000

    Refactoring; rename

--- test_server/util/run.sh
+++ test_server/util/run.sh
@@ -4,15 +4,16 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly TEST_FILES=(${MY_DIR}/../*_test.rb)
 readonly TEST_ARGS=(${*})
 readonly TEST_LOG=${COVERAGE_ROOT}/test.log
-readonly TEST_LOG_TMP=${COVERAGE_ROOT}/test.log.tmp
+readonly TEST_LOG_PART=${COVERAGE_ROOT}/test.log.part
 
 readonly SCRIPT="([ '${MY_DIR}/coverage.rb' ] + %w(${TEST_FILES[*]})).each{ |file| require file }"
 
 export RUBYOPT='-W2'
 mkdir -p ${COVERAGE_ROOT}
-ruby -e "${SCRIPT}" -- ${TEST_ARGS[@]} 2>&1 | tee ${TEST_LOG} ${TEST_LOG_TMP}
+ruby -e "${SCRIPT}" -- ${TEST_ARGS[@]} \
+  2>&1 | tee ${TEST_LOG} ${TEST_LOG_PART}
 
 ruby ${MY_DIR}/check_test_results.rb \
-  ${TEST_LOG_TMP} \
+  ${TEST_LOG_PART} \
   ${COVERAGE_ROOT}/index.html \
-    | tee -a ${TEST_LOG}
+    2>&1 | tee -a ${TEST_LOG}
