b795b0717a36e785f9adfb5e969d5c1550411f93
Jon Jagger <jon@jaggersoft.com>
2019-12-21 14:19:11 +0000

    Rely on dependencies in docker-compose.yml and simplify containers_up.sh

--- sh/containers_up.sh
+++ sh/containers_up.sh
@@ -13,50 +13,47 @@ wait_briefly_until_ready()
   local -r max_tries=10
   printf "Waiting until ${name} is ready"
   for _ in $(seq ${max_tries}); do
-    printf .
-    if ready ${port}; then
-      printf 'OK\n'
+    if curl_ready ${port}; then
+      printf '.OK\n'
       return
     else
+      printf .
       sleep 0.1
     fi
   done
   printf 'FAIL\n'
-  printf "${name} not ready after ${max_tries} tries\n"
-  if [ -f "$(ready_response_filename)" ]; then
-    printf "$(ready_response)\n"
+  echo "not ready after ${max_tries} tries"
+  if [ -f "$(ready_filename)" ]; then
+    ready_response
   fi
   docker logs ${name}
   exit 42
 }
 
 # - - - - - - - - - - - - - - - - - - -
-ready()
+curl_ready()
 {
   local -r port="${1}"
-  local -r path=ready
-  local -r ready_cmd="\
-    curl \
-      --output $(ready_response_filename) \
-      --silent \
-      --fail \
-      -X GET http://${IP_ADDRESS}:${port}/${path}"
-  rm -f "$(ready_response_filename)"
-  if ${ready_cmd} && [ "$(ready_response)" = '{"ready?":true}' ]; then
-    true
-  else
-    false
-  fi
+  local -r path=ready?
+  local -r url="http://${IP_ADDRESS}:${port}/${path}"
+  rm -f "$(ready_filename)"
+  curl \
+    --fail \
+    --output $(ready_filename) \
+    --silent \
+    -X GET \
+    "${url}"
+  [ "$?" == '0' ] && [ "$(ready_response)" == '{"ready?":true}' ]
 }
 
 # - - - - - - - - - - - - - - - - - - -
 ready_response()
 {
-  cat "$(ready_response_filename)"
+  cat "$(ready_filename)"
 }
 
 # - - - - - - - - - - - - - - - - - - -
-ready_response_filename()
+ready_filename()
 {
   printf /tmp/curl-custom-ready-output
 }
@@ -66,7 +63,7 @@ exit_unless_clean()
 {
   local -r name="${1}"
   local -r docker_log=$(docker logs "${name}" 2>&1)
-  local -r line_count=$(echo -n "${docker_log}" | grep -c '^')
+  local -r line_count=$(echo -n "${docker_log}" | grep --count '^')
   printf "Checking ${name} started cleanly..."
   # 3 lines on Thin (Unicorn=6, Puma=6)
   #Thin web server (v1.7.2 codename Bachmanity)
@@ -76,20 +73,20 @@ exit_unless_clean()
     printf 'OK\n'
   else
     printf 'FAIL\n'
-    print_docker_log "${name}" "${docker_log}"
+    echo_docker_log "${name}" "${docker_log}"
     exit 42
   fi
 }
 
 # - - - - - - - - - - - - - - - - - - -
-print_docker_log()
+echo_docker_log()
 {
   local -r name="${1}"
   local -r docker_log="${2}"
-  printf "[docker logs ${name}]\n"
-  printf '<docker_log>\n'
-  printf "${docker_log}\n"
-  printf '</docker_log>\n'
+  echo "[docker logs ${name}]"
+  echo '<docker_log>'
+  echo "${docker_log}"
+  echo '</docker_log>'
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -120,6 +117,4 @@ container_up()
 
 # - - - - - - - - - - - - - - - - - - -
 export NO_PROMETHEUS=true
-container_up_ready_and_clean 4537 saver
 container_up_ready_and_clean ${CYBER_DOJO_CREATOR_PORT} creator-server
-container_up_ready_and_clean 4526 custom-start-points
