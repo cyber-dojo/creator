bbd43b1ce1a7f7a4085aaa66e7064d0effc5927b
Jon Jagger <jon@jaggersoft.com>
2020-02-14 15:49:01 +0000

    Fix script expression (different on CI)

--- sh/ip_address.sh
+++ sh/ip_address.sh
@@ -3,7 +3,7 @@ set -e
 
 ip_address()
 {
-  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
+  if [ -n "${DOCKER_MACHINE_NAME:-}" ]; then
     docker-machine ip ${DOCKER_MACHINE_NAME}
   else
     printf localhost
