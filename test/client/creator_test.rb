46dd71ec36742041e9c1da874148017989aacaac
Jon Jagger <jon@jaggersoft.com>
2020-02-14 09:05:06 +0000

    Add client-side tests for rainy-day scenarios

--- test/client/creator_test.rb
+++ test/client/creator_test.rb
@@ -10,7 +10,7 @@ class CreatorTest < CreatorTestBase
   # - - - - - - - - - - - - - - - - -
 
   test '702',
-  %w( /create_group returns the id of a newly created group ) do
+  %w( create_group returns the id of a newly created group ) do
     id = creator.create_group(any_manifest)
     assert group_exists?(id), id
   end
@@ -18,9 +18,36 @@ class CreatorTest < CreatorTestBase
   # - - - - - - - - - - - - - - - - -
 
   test '703',
-  %w( /create_kata returns the id of a newly created kata ) do
+  %w( create_kata returns the id of a newly created kata ) do
     id = creator.create_kata(any_manifest)
     assert kata_exists?(id), id
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '45e',
+  %w( create_group with a null manifest raises ) do
+    assert_raises(Creator::Error) {
+      creator.create_group(nil)
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '45a',
+  %w( custom.manifest(unknown_name) raises ) do
+    assert_raises(CustomStartPoints::Error) {
+      custom.manifest('unknown-name')
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '45b',
+  %w( saver.exists?(nil) raises ) do
+    assert_raises(Saver::Error) {
+      saver.exists?(nil)
+    }
   end
 
 end
