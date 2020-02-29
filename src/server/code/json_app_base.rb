76b264b1f97fe3f366cca38aad9f7e0f57f3ed1a
Jon Jagger <jon@jaggersoft.com>
2020-02-29 20:23:20 +0000

    Refactoring

--- src/server/code/json_app_base.rb
+++ src/server/code/json_app_base.rb
@@ -13,50 +13,6 @@ class JsonAppBase < Sinatra::Base
 
   silently { register Sinatra::Contrib }
   set :port, ENV['PORT']
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  def self.get_json(name)
-    get "/#{name}", provides:[:json] do
-      respond_to do |format|
-        format.json {
-          result = instance_eval {
-            target.public_send(name, **args)
-          }
-          json({ name => result })
-        }
-      end
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  def self.post_json(name)
-    post "/#{name}", provides:[:json] do
-      respond_to do |format|
-        format.json {
-          result = instance_eval {
-            target.public_send(name, **args)
-          }
-          new_api = { name => result }
-          backwards_compatible = { id:result }
-          json new_api.merge(backwards_compatible)
-        }
-      end
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  def self.get_probe(name)
-    get "/#{name}" do
-      result = instance_eval { target.public_send(name) }
-      json({ name => result })
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
   set :show_exceptions, false
 
   error do
@@ -91,17 +47,56 @@ class JsonAppBase < Sinatra::Base
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
-  def args
-    payload = json_hash_parse(request.body.read)
-    x = Hash[payload.map{ |key,value| [key.to_sym, value] }]
-    #dump_args(x)
-    x
+  def self.get_json(name)
+    get "/#{name}", provides:[:json] do
+      respond_to do |format|
+        format.json {
+          result = instance_eval {
+            target.public_send(name, **json_args)
+          }
+          json({ name => result })
+        }
+      end
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - -
+
+  def self.post_json(name)
+    post "/#{name}", provides:[:json] do
+      respond_to do |format|
+        format.json {
+          result = instance_eval {
+            target.public_send(name, **json_args)
+          }
+          new_api = { name => result }
+          backwards_compatible = { id:result }
+          json new_api.merge(backwards_compatible)
+        }
+      end
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - -
+
+  def self.get_probe(name)
+    get "/#{name}" do
+      result = instance_eval { target.public_send(name) }
+      json({ name => result })
+    end
   end
 
   private
 
+  def json_args
+    Hash[payload.map{ |key,value| [key.to_sym, value] }]
+  end
+
+  def payload
+    json_hash_parse(request.body.read)
+  end
+
   def json_hash_parse(body)
-    #dump_payload(body)
     json = (body === '') ? {} : JSON.parse!(body)
     unless json.instance_of?(Hash)
       fail 'body is not JSON Hash'
@@ -110,23 +105,5 @@ class JsonAppBase < Sinatra::Base
   rescue JSON::ParserError
     fail 'body is not JSON'
   end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-=begin
-  def dump_payload(body)
-    if request.path === '/sha'
-      puts "request.content_type:#{request.content_type}:"
-      puts "params:#{params}:"
-      puts "body:#{body}:"
-    end
-  end
-
-  def dump_args(args)
-    if request.path === '/sha'
-      puts "args (for **)==#{args}"
-    end
-  end
-=end
 
 end
