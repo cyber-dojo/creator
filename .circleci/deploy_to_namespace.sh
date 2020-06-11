66b5ce4210b456ca4e4a447c019808044f57a640
Jon Jagger <jon@jaggersoft.com>
2020-06-11 19:08:32 +0100

    Provide :- default to optional helm_update argument

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -31,11 +31,8 @@ readonly NAMESPACE="${1}" # beta|prod
 readonly CYBER_DOJO_CREATOR_TAG="${CIRCLE_SHA1:0:7}"
 
 helm_upgrade \
-   "${NAMESPACE}" \
+   "${NAMESPACE}" "creator" "praqma/cyber-dojo-service --version 0.2.5"
    "${CYBER_DOJO_CREATOR_IMAGE}" \
    "${CYBER_DOJO_CREATOR_TAG}" \
    "${CYBER_DOJO_CREATOR_PORT}" \
-   ".circleci/k8s-general-values.yml" \
-   ".circleci/k8s-specific-values.yml" \
-   "creator" \
-   "praqma/cyber-dojo-service --version 0.2.5"
+   ".circleci/k8s-general-values.yml"
