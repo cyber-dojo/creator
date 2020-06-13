9996f71dc5a28a2cd25f651b9b3d4057f17ae405
Jon Jagger <jon@jaggersoft.com>
2020-06-13 10:49:40 +0100

    Source deployment functions from single curled file; move export command before setting tag variable

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -Eeu
 
 readonly NAMESPACE="${1}" # beta|prod
+readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
+readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+source <(curl "${K8S_URL}/sh/deployment_functions.sh")
+export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_CREATOR_TAG="${CIRCLE_SHA1:0:7}"
-readonly SCRIPT_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master/sh
-source <(curl "${SCRIPT_URL}/gcloud_init.sh")
-source <(curl "${SCRIPT_URL}/helm_init.sh")
-source <(curl "${SCRIPT_URL}/helm_upgrade_probe_yes_prometheus_yes.sh")
-export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
 
 gcloud_init
 helm_init
