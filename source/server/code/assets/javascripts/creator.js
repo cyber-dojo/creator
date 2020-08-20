0d82adcaae31390d9f0619d8fa3d6177b06a4530
Jon Jagger <jon@jaggersoft.com>
2020-08-20 16:47:02 +0100

    Move display-names JS handler into assets file

--- source/server/code/assets/javascripts/creator.js
+++ source/server/code/assets/javascripts/creator.js
@@ -1,2 +1,19 @@
 
 const cd = {};
+
+cd.setupDisplayNamesClickHandlers = () => {
+  const $displayNames = $('.display-name');
+  const $displayContent = $('.display-content');
+  $displayNames.click((event) => {
+    const $element = $(event.target);
+    cd.selectedDisplayName = $element.data('name').trim();
+    $displayNames.removeClass('selected');
+    $element.addClass('selected');
+    const index = $element.data('index');
+    const content = $(`#contents_${index}`).val();
+    $displayContent.val(content);
+  });
+  const random = (n) => Math.floor(Math.random() * n);
+  const any = random($displayNames.length);
+  $displayNames[any].click().scrollIntoView();
+};
