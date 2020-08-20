9498fa96635da2c5b8bd8f9fd5a98039cff097a4
Jon Jagger <jon@jaggersoft.com>
2020-08-20 16:23:16 +0100

    Refactoring; test tidying

--- test/server/assets_test.rb
+++ test/server/assets_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require_relative 'creator_test_base'
 
-class CssAssetsTest < CreatorTestBase
+class AssetsTest < CreatorTestBase
 
   def self.id58_prefix
     'Q3p'
@@ -15,6 +15,16 @@ class CssAssetsTest < CreatorTestBase
     get '/assets/app.css'
     assert status?(200), status
     assert css_content?, content_type
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '3Je', %w(
+  |GET /assets/app.js is served
+  ) do
+    get '/assets/app.js'
+    assert status?(200), status
+    assert js_content?, content_type
   end
 
 end
