590c06a8315d197f7ecfaf74b63e1c2af8fda7ff
Jon Jagger <jon@jaggersoft.com>
2020-10-08 20:24:50 +0100

    Set proper client-side test metrics

--- test/client/metrics.rb
+++ test/client/metrics.rb
@@ -1,10 +1,10 @@
 
 MIN = {
   test_count:1,
-  app_coverage:97,
-  test_coverage:97,
+  app_coverage:100,
+  test_coverage:100,
   line_ratio:1.2,
-  hits_ratio:0.8
+  hits_ratio:0.5
 }
 
 MAX = {
@@ -12,5 +12,5 @@ MAX = {
   errors:0,
   warnings:2,
   skips:0,
-  duration:20,
+  duration:40,
 }
