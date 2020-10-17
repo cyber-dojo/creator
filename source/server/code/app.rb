2c557f2f917b40470abacdb794c423b3bc51019f
Jon Jagger <jon@jaggersoft.com>
2020-10-17 11:24:27 +0100

    Use single/group buttons to match existing home page

--- source/server/code/app.rb
+++ source/server/code/app.rb
@@ -30,8 +30,23 @@ class App < AppBase
   get '/home', provides:[:html] do
     respond_to do |format|
       format.html do
-        @id = params['id'] || ''
         erb :home
+      end
+    end
+  end
+
+  get '/group', provides:[:html] do
+    respond_to do |format|
+      format.html do
+        erb :group
+      end
+    end
+  end
+
+  get '/single', provides:[:html] do
+    respond_to do |format|
+      format.html do
+        erb :single
       end
     end
   end
