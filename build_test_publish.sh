1c8bc8d8c77fbe0f48c0f09a30e0f3e455b40ef2
Jon Jagger <jon@jaggersoft.com>
2020-11-28 13:58:44 +0000

    Added debug echos for tracking down client-side dns lookup failure.

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -7,7 +7,6 @@ source "${SH_DIR}/build_tagged_images.sh"
 source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/copy_in_saver_test_data.sh"
-source "${SH_DIR}/echo_versioner_env_vars.sh"
 source "${SH_DIR}/exit_non_zero_unless_installed.sh"
 source "${SH_DIR}/exit_zero_if_build_only.sh"
 source "${SH_DIR}/exit_zero_if_show_help.sh"
@@ -15,6 +14,7 @@ source "${SH_DIR}/on_ci_publish_tagged_images.sh"
 source "${SH_DIR}/remove_old_images.sh"
 source "${SH_DIR}/test_in_containers.sh"
 
+source "${SH_DIR}/echo_versioner_env_vars.sh"
 export $(echo_versioner_env_vars)
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
@@ -27,6 +27,13 @@ exit_zero_if_build_only "$@"
 server_up_healthy_and_clean
 client_up_healthy_and_clean "$@"
 copy_in_saver_test_data
+
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+cat /tmp/augmented-docker-compose.creator.peek.yml
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+docker ps -a
+docker logs test_nginx
+
 test_in_containers "$@"
 containers_down
 on_ci_publish_tagged_images
