755208a578096dd0d228515f700f0d767e8131ea
Jon Jagger <jon@jaggersoft.com>
2020-11-28 13:41:06 +0000

    Fixed incorrect USER settings in docker-compose.yml

--- sh/augmented_docker_compose.sh
+++ sh/augmented_docker_compose.sh
@@ -3,6 +3,11 @@
 # cyberdojo/service-yaml image lives at
 # https://github.com/cyber-dojo-tools/service-yaml
 
+# The initial change-directory command is needed because
+# the current working directory is taken as the dir for
+# relative pathnames (eg in volume-mounts) when the
+# yml is received from stdin (--file -).
+
 augmented_docker_compose()
 {
   cd "${ROOT_DIR}" && cat "./docker-compose.yml" \
