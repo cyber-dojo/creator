2ab44d302ade761b1f2c95ee5a35d1cc4011763f
Jon Jagger <jon@jaggersoft.com>
2020-11-28 13:27:11 +0000

    Added build script checks that docker and docker-compose are installed.

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -8,6 +8,7 @@ source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/copy_in_saver_test_data.sh"
 source "${SH_DIR}/echo_versioner_env_vars.sh"
+source "${SH_DIR}/exit_non_zero_unless_installed.sh"
 source "${SH_DIR}/exit_zero_if_build_only.sh"
 source "${SH_DIR}/exit_zero_if_show_help.sh"
 source "${SH_DIR}/on_ci_publish_tagged_images.sh"
@@ -18,6 +19,8 @@ export $(echo_versioner_env_vars)
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 exit_zero_if_show_help "$@"
+exit_non_zero_unless_installed docker
+exit_non_zero_unless_installed docker-compose
 remove_old_images
 build_tagged_images
 exit_zero_if_build_only "$@"
