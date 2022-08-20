016caa7fade7752f7cb6fae6d00f3d2fa1190746
Jon Jagger <jon@jaggersoft.com>
2022-08-20 17:34:48 +0100

    CI: fix .circleci/merkely_log_deployment.sh

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -Eeu
 
-MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 readonly MERKELY_CHANGE=merkely/change:latest
 readonly MERKELY_OWNER=cyber-dojo
 readonly MERKELY_PIPELINE=creator
 
 # - - - - - - - - - - - - - - - - - - -
-merkely_fingerprint()
+kosli_fingerprint()
 {
   echo "docker://${CYBER_DOJO_CREATOR_IMAGE}:${CYBER_DOJO_CREATOR_TAG}"
 }
 
 # - - - - - - - - - - - - - - - - - - -
-merkely_log_deployment()
+kosli_log_deployment()
 {
   local -r environment="${1}"
   local -r hostname="${2}"
@@ -22,7 +20,7 @@ merkely_log_deployment()
     --env MERKELY_COMMAND=log_deployment \
     --env MERKELY_OWNER=${MERKELY_OWNER} \
     --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
+    --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
     --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
     --env MERKELY_ENVIRONMENT="${environment}" \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
@@ -33,6 +31,7 @@ merkely_log_deployment()
       ${MERKELY_CHANGE}
 }
 
+# - - - - - - - - - - - - - - - - - - -
 VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 export $(curl "${VERSIONER_URL}/app/.env")
 export CYBER_DOJO_CREATOR_TAG="${CIRCLE_SHA1:0:7}"
