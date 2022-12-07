ddf75548ae12ade07c0648d798ec8b42bfd5ef45
Ewelina Wilkosz <ewelina@kosli.com>
2022-12-07 11:52:41 +0100

    CI: delete unused code and update reusable workflow version

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -22,15 +22,6 @@ kosli_declare_pipeline()
 {
   local -r hostname="${1}"
 
-	# docker run \
-	# 	--env MERKELY_COMMAND=declare_pipeline \
-  #   --env MERKELY_OWNER=${MERKELY_OWNER} \
-  #   --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-	# 	--env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-  #   --env MERKELY_HOST="${hostname}" \
-	# 	--rm \
-	# 	--volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
-	# 	  ${MERKELY_CHANGE}
   kosli pipeline declare \
     --description "UX for Group/Kata creation" \
     --visibility public \
@@ -42,22 +33,6 @@ kosli_declare_pipeline()
 kosli_log_artifact()
 {
   local -r hostname="${1}"
-
-	# docker run \
-  #   --env MERKELY_COMMAND=log_artifact \
-  #   --env MERKELY_OWNER=${MERKELY_OWNER} \
-  #   --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-  #   --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
-  #   --env MERKELY_IS_COMPLIANT=TRUE \
-  #   --env MERKELY_ARTIFACT_GIT_COMMIT=${CYBER_DOJO_CREATOR_SHA} \
-  #   --env MERKELY_ARTIFACT_GIT_URL=https://github.com/${MERKELY_OWNER}/${MERKELY_PIPELINE}/commit/${CYBER_DOJO_CREATOR_SHA} \
-  #   --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
-  #   --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-  #   --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-  #   --env MERKELY_HOST="${hostname}" \
-  #   --rm \
-  #   --volume /var/run/docker.sock:/var/run/docker.sock \
-  #     ${MERKELY_CHANGE}
   
   kosli pipeline artifact report creation ${CYBER_DOJO_CREATOR_IMAGE}:${CYBER_DOJO_CREATOR_TAG} \
     --repo-root ../../.. \
@@ -68,23 +43,6 @@ kosli_log_artifact()
 kosli_log_evidence()
 {
   local -r hostname="${1}"
-
-	# docker run \
-  #   --env MERKELY_COMMAND=log_evidence \
-  #   --env MERKELY_OWNER=${MERKELY_OWNER} \
-  #   --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-  #   --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
-  #   --env MERKELY_EVIDENCE_TYPE=branch-coverage \
-  #   --env MERKELY_IS_COMPLIANT=TRUE \
-  #   --env MERKELY_DESCRIPTION="server & client branch-coverage reports" \
-  #   --env MERKELY_USER_DATA="$(evidence_json_path)" \
-  #   --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-  #   --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-  #   --env MERKELY_HOST="${hostname}" \
-  #   --rm \
-  #   --volume "$(evidence_json_path):$(evidence_json_path)" \
-  #   --volume /var/run/docker.sock:/var/run/docker.sock \
-  #     ${MERKELY_CHANGE}
 
   kosli pipeline artifact report evidence generic ${CYBER_DOJO_CREATOR_IMAGE}:${CYBER_DOJO_CREATOR_TAG} \
     --description "server & client branch-coverage reports" \
