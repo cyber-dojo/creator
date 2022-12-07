a21e482040a9ea7b2d629b31ea64730f2ddec57b
Ewelina Wilkosz <ewelina@kosli.com>
2022-12-07 13:21:08 +0100

    CI: use kosli cli in deployment steps

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -4,6 +4,11 @@ readonly MERKELY_CHANGE=merkely/change:latest
 readonly MERKELY_OWNER=cyber-dojo
 readonly MERKELY_PIPELINE=creator
 
+export KOSLI_OWNER=cyber-dojo
+export KOSLI_PIPELINE=creator
+export KOSLI_API_TOKEN=${MERKELY_API_TOKEN}
+export KOSLI_ARTIFACT_TYPE=docker
+
 # - - - - - - - - - - - - - - - - - - -
 kosli_fingerprint()
 {
@@ -16,19 +21,25 @@ kosli_log_deployment()
   local -r environment="${1}"
   local -r hostname="${2}"
 
-	docker run \
-    --env MERKELY_COMMAND=log_deployment \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
-    --env MERKELY_DESCRIPTION="Deployed to ${environment} in Github Actions pipeline" \
-    --env MERKELY_ENVIRONMENT="${environment}" \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST="${hostname}" \
-    --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-      ${MERKELY_CHANGE}
+	# docker run \
+  #   --env MERKELY_COMMAND=log_deployment \
+  #   --env MERKELY_OWNER=${MERKELY_OWNER} \
+  #   --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+  #   --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
+  #   --env MERKELY_DESCRIPTION="Deployed to ${environment} in Github Actions pipeline" \
+  #   --env MERKELY_ENVIRONMENT="${environment}" \
+  #   --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+  #   --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+  #   --env MERKELY_HOST="${hostname}" \
+  #   --rm \
+  #   --volume /var/run/docker.sock:/var/run/docker.sock \
+  #     ${MERKELY_CHANGE}
+
+  kosli expect deployment ${CYBER_DOJO_CREATOR_IMAGE}:${CYBER_DOJO_CREATOR_TAG} \
+    --description "Deployed to ${environment} in Github Actions pipeline" \
+    --environment ${environment} \
+    --host ${hostname} 
+
 }
 
 # - - - - - - - - - - - - - - - - - - -
