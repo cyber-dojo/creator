e9f60f9ed175b4f499a2ecaea81ccb2de4ea4912
Ewelina Wilkosz <ewelina@kosli.com>
2022-12-07 11:21:41 +0100

    CI: artifact creation logged with kosli cli

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -42,21 +42,24 @@ kosli_log_artifact()
 {
   local -r hostname="${1}"
 
-	docker run \
-    --env MERKELY_COMMAND=log_artifact \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
-    --env MERKELY_IS_COMPLIANT=TRUE \
-    --env MERKELY_ARTIFACT_GIT_COMMIT=${CYBER_DOJO_CREATOR_SHA} \
-    --env MERKELY_ARTIFACT_GIT_URL=https://github.com/${MERKELY_OWNER}/${MERKELY_PIPELINE}/commit/${CYBER_DOJO_CREATOR_SHA} \
-    --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST="${hostname}" \
-    --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-      ${MERKELY_CHANGE}
+	# docker run \
+  #   --env MERKELY_COMMAND=log_artifact \
+  #   --env MERKELY_OWNER=${MERKELY_OWNER} \
+  #   --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+  #   --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
+  #   --env MERKELY_IS_COMPLIANT=TRUE \
+  #   --env MERKELY_ARTIFACT_GIT_COMMIT=${CYBER_DOJO_CREATOR_SHA} \
+  #   --env MERKELY_ARTIFACT_GIT_URL=https://github.com/${MERKELY_OWNER}/${MERKELY_PIPELINE}/commit/${CYBER_DOJO_CREATOR_SHA} \
+  #   --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
+  #   --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+  #   --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+  #   --env MERKELY_HOST="${hostname}" \
+  #   --rm \
+  #   --volume /var/run/docker.sock:/var/run/docker.sock \
+  #     ${MERKELY_CHANGE}
+  kosli pipeline artifact report creation ${CYBER_DOJO_CREATOR_IMAGE}:${CYBER_DOJO_CREATOR_TAG} \
+    --artifact-type docker \
+    --host "${hostname}"
 }
 
 # - - - - - - - - - - - - - - - - - - -
