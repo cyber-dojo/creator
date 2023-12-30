c7c958668cc8c1b755fd431e39869364f5f79b73
JonJagger <jon@kosli.com>
2023-12-30 20:09:41 +0000

    Fix echo_versioner_env_vars.sh

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -1,10 +1,13 @@
 
 echo_versioner_env_vars()
 {
+  local -r commit_sha="$(cd "$(repo_root)" && git rev-parse HEAD)"
+  local -r image_tag="${commit_sha:0:7}"
+
   docker run --rm cyberdojo/versioner:latest
   #
-  echo CYBER_DOJO_CREATOR_SHA="$(git_commit_sha)"
-  echo CYBER_DOJO_CREATOR_TAG="$(get_image_tag)"
+  echo CYBER_DOJO_CREATOR_SHA="${commit_sha}"
+  echo CYBER_DOJO_CREATOR_TAG="${image_tag}"
   #
   echo CYBER_DOJO_CREATOR_CLIENT_IMAGE=cyberdojo/client
   echo CYBER_DOJO_CREATOR_CLIENT_PORT=9999
