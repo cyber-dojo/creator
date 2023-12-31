b0fd9e76e5fdca784a35ff9e29971a87108dabdb
JonJagger <jon@kosli.com>
2023-12-31 05:34:03 +0000

    Simplify test/coverage.rb [ci skip]

--- test/coverage.rb
+++ test/coverage.rb
@@ -1,17 +1,13 @@
 require 'simplecov'
 require_relative 'simplecov-json'
 
-$CODE_DIR = ENV['CODE_DIR']
-$TEST_DIR = ENV['TEST_DIR']
-
 SimpleCov.start do
   enable_coverage(:branch)
   filters.clear
-  add_filter("/usr/")
   coverage_dir(ENV['COVERAGE_ROOT'])
   #add_group('debug') { |src| puts(src.filename); false }
-  add_group($CODE_DIR) { |src| src.filename !~ %r"test" }
-  add_group($TEST_DIR) { |src| src.filename =~ %r"test" }
+  add_group('app')  { |src| src.filename !~ %r"test" }
+  add_group('test') { |src| src.filename =~ %r"test" }
 end
 
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
